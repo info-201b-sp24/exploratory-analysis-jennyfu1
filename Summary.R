a0ad28118d4290b41eebec3bc8004458fa8597cc
cyinyu <cyinyu@uw.edu>
2024-05-17 14:53:46 -0700

    commit message

--- Summary.R
+++ Summary.R
@@ -1,6 +1,7 @@
 library(dplyr)
 covid <- read.csv("/Users/yinyuchen/Desktop/info201/Covid_Data.csv")
 View(covid)
+
 summary_info <- list()
 summary_info$num_patients <- nrow(covid)
 summary_info$num_features <- ncol(covid)
@@ -20,6 +21,4 @@ summary_info$num_died <- covid %>%
   filter(DATE_DIED != as.Date(9999-99-99)) %>%
   nrow()
 
-
-View(summary_info)
 summary_info
