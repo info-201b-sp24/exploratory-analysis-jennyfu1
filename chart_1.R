ce26ef53bd27139d482ab88c47951ae468b095b9
jennyfu1 <jennyfu1@uw.edu>
2024-05-17 18:28:57 -0700

    commit message

--- chart_1.R
+++ chart_1.R
@@ -1,23 +1,23 @@
 library(dplyr)
 Covid <- read.csv("/Users/jenny/Desktop/Covid Data.csv")
 library(ggplot2)
-data("Covid", package = "ggplot2")
 
-Covid <- Covid %>% mutate(TOBACCO=recode(TOBACCO, `1`="Yes", `2`="No"))
 
 chart1_1 <- Covid %>% 
-  filter(CLASIFFICATION_FINAL<4) 
+  filter(CLASIFFICATION_FINAL<4) %>% 
+  filter(TOBACCO<3)
 chart1_2 <- Covid %>% 
-  filter(CLASIFFICATION_FINAL>3) 
+  filter(CLASIFFICATION_FINAL>3) %>%
+  filter(TOBACCO<3)
 
 
-ggplot(data=chart1_1,aes(x=TOBACCO))+
+ggplot(data=chart1_1,aes(x=factor(TOBACCO, levels = c(1, 2), labels = c("Yes", "No"))))+
   geom_bar()+
   ylim(c(0,400000))+
   labs(title="The distribution of tobacco use", 
        subtitle="people with COVID")
 
-ggplot(data=chart1_2,aes(x=TOBACCO))+
+ggplot(data=chart1_2,aes(x=factor(TOBACCO, levels = c(1, 2), labels = c("Yes", "No"))))+
   geom_bar()+
   ylim(c(0,400000))+
   labs(title="The distribution of tobacco use", 
